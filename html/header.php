c6d4fe7a8ec61ce06a4fac4f61f7ad4508a1b5ff
puneeth09 <51479315+puneeth09@users.noreply.github.com>
2019-06-23 23:26:02 +0530

    Add files via upload

---
+++ html/header.php
@@ -0,0 +1,45 @@
+<!Doctype html>
+<html>
+<head>
+    <title>
+        Blog
+    </title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!--for optimisation-->
+    <meta charset="utf=8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="description" content="A blog">
+    <link href="js/bootstrap.js">
+    </head>
+<body>
+    <!--Nav bar-->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Welcome</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExample04">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="album.php">Albums</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="signin.php">sign in</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">more</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown04">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
